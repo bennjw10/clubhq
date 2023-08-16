fbd3bb57f88a02db620a572e537779dd3495bc83
Benn Pickering <pickering.benn@gmail.com>
2023-08-16 18:48:13 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+
+#create a flask instance
+app = Flask(__name__)
+
+#create a route decorator
+@app.route ('/')
+
+
+def index():
+    first_name = "Benn"
+    stuff = "this is <strong>bold</strong> text"
+    return render_template("index.html", first_name=first_name, stuff=stuff)
+
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", user_name=name)
+
